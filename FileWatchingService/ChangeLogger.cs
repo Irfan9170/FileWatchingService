554645635650359125394c5e21a1702f52db0b73
Irfan <irfanali987654@gmail.com>
2022-06-04 11:14:58 +0530

    Add project files.

---
+++ FileWatchingService/ChangeLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWatchingService
+{
+    public static class ChangeLogger
+    {
+        public static void Log(string message)
+        {
+            try
+            {
+                string _message = String.Format("{0} {1}", message, Environment.NewLine);
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "logFile.txt",message);
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+    }
+}
---
+++ FileWatchingService/FileWatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatchingService
+{
+    public class FileWatcher
+    {
+        private FileSystemWatcher _fileWatcher;
+        public FileWatcher()
+        {
+            _fileWatcher = new FileSystemWatcher(@"F:\learning");
+            _fileWatcher.Created += new FileSystemEventHandler(_fileWatcher_Created);
+            _fileWatcher.EnableRaisingEvents = true;
+
+        }
+
+        void _fileWatcher_Created(object sender,FileSystemEventArgs e)
+        {
+            ChangeLogger.Log(String.Format("File Changed: Path:{0},Name:{1}",e.FullPath,e.Name));
+        }
+    }
+}
